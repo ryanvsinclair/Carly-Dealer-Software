adcfe9cbd3c981f54769899c4fa0f7df59b5b4a3
ryanvsinclair <ryanvsinclair@gmail.com>
2026-01-13 07:51:14 +0000

    bla

--- src/app/(dealer)/[dealershipId]/team/page.tsx
+++ src/app/(dealer)/[dealershipId]/team/page.tsx
@@ -94,7 +94,7 @@ export default async function DealerTeamPage({
           <TableBody>
             {team?.map((member) => (
               <TableRow
-                key={member.membership_id}
+                key={member.user_id}
                 className="border-b border-[#E5E5E5] dark:border-[#262626]"
               >
                 <TableCell className="text-[14px] font-light text-[#171717] dark:text-[#FAFAFA]">
